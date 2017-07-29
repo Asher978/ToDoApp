15b9183b90cd3eb27f9dd9b5e31012e2fa14c9f8
Asher Shaheen <ashershaheen@gmail.com>
2017-07-28 20:17:08 -0400

    added the method & route for by id

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -2,20 +2,31 @@ const Todo = require('../models/todo');
 
 const todoController = {};
 
-
+// show all
 todoController.index = (req, res) => {
     Todo.findAll()
     .then(todo => {
         res.render('todo/todo-index', {
-            message: 'ok',
+            currentPage: 'index page',
             data: todo
         })
     }).catch(err => {
         console.log(err);
-        res.status(500).json({
-            message: 'Not Found :((',
-            error: err,
+        res.status(500).json(err);
+    });
+};
+
+//by id
+todoController.show = (req, res) => {
+    Todo.findById(req.params.id)
+    .then(todo => {
+        res.render('todo/todo-single', {
+            currentPage: 'single page',
+            data: todo,
         });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
     });
 };
 
--- models/todo.js
+++ models/todo.js
@@ -2,9 +2,17 @@ const db = require('../db/config');
 
 const Todo = {};
 
+//showing all
 Todo.findAll = () => {
     return db.query('SELECT * FROM todo')
 }
 
+//showing by ID
+Todo.findById = (id) => {
+    return db.oneOrNone(`
+        SELECT * FROM todo
+        WHERE id = $1
+    `, [id]);
+}
 
 module.exports = Todo;
