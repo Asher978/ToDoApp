dffd4ae726fee91c6498b1dfed96e6f0d5c19050
Asher Shaheen <ashershaheen@gmail.com>
2017-07-29 22:09:30 -0400

    altered the migration to add another table for user's info

--- db/migrations/migration.sql
+++ db/migrations/migration.sql
@@ -8,3 +8,15 @@ CREATE TABLE IF NOT EXISTS todo (
     category VARCHAR(400),
     description VARCHAR(1024)
 );
+
+CREATE  TABLE IF NOT EXISTS users (
+    id SERIAl PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_digest TEXT NOT NULL,
+    firstname VARCHAR(255),
+    lastname VARCHAR(255)
+);
+
+ALTER TABLE todo
+ADD COLUMN user_id INTEGER REFERENCES users(id);
