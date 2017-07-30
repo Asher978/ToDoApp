abe8e5dec1e032e80755a86ec3dde26ae80946a4
Asher Shaheen <ashershaheen@gmail.com>
2017-07-29 22:14:49 -0400

    required packages that are needed to setup auth

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
+require('dotenv').config();
 app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //static
 app.use(express.static('public'))
