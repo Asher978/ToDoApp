eeef949640496041cb45f75785dbedad40122135
Asher Shaheen <ashershaheen@gmail.com>
2017-07-29 22:40:00 -0400

    added the login redirect function

--- services/auth/auth-helpers.js
+++ services/auth/auth-helpers.js
@@ -4,3 +4,8 @@ const User = require('../../models/user');
 function comparePass(userPassword, databasePassword) {
     return bcrypt.compareSync(userPassword, databasePassword);
 }
+
+function loginRedirect (req, res, next) {
+    if (req.users) return res.redirect('/user');
+    return next();
+}
